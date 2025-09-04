e744761d898d5aa691acb5a4e22d706d9ecfe8ac
Akira Ishino <akira@lumilab.jp>
2025-09-04 20:59:24 +0900

    display agents

---
+++ src/display.rs
@@ -0,0 +1,114 @@
+use std::vec;
+
+use agent_stream_kit::{
+    ASKit, AgentConfig, AgentContext, AgentData, AgentDefinition, AgentDisplayConfigEntry,
+    AgentError, AgentOutput, AgentValue, AgentValueMap, AsAgent, AsAgentData, new_boxed,
+};
+
+// Display Data
+struct DisplayDataAgent {
+    data: AsAgentData,
+}
+
+impl AsAgent for DisplayDataAgent {
+    fn new(
+        askit: ASKit,
+        id: String,
+        def_name: String,
+        config: Option<AgentConfig>,
+    ) -> Result<Self, AgentError> {
+        Ok(Self {
+            data: AsAgentData::new(askit, id, def_name, config),
+        })
+    }
+
+    fn data(&self) -> &AsAgentData {
+        &self.data
+    }
+
+    fn mut_data(&mut self) -> &mut AsAgentData {
+        &mut self.data
+    }
+
+    fn start(&mut self) -> Result<(), AgentError> {
+        Ok(())
+    }
+
+    fn process(&mut self, _ctx: AgentContext, data: AgentData) -> Result<(), AgentError> {
+        self.emit_display(DISPLAY_DATA, data);
+        Ok(())
+    }
+}
+
+// Debug Data
+struct DebugDataAgent {
+    data: AsAgentData,
+}
+
+impl AsAgent for DebugDataAgent {
+    fn new(
+        askit: ASKit,
+        id: String,
+        def_name: String,
+        config: Option<AgentConfig>,
+    ) -> Result<Self, AgentError> {
+        Ok(Self {
+            data: AsAgentData::new(askit, id, def_name, config),
+        })
+    }
+
+    fn data(&self) -> &AsAgentData {
+        &self.data
+    }
+
+    fn mut_data(&mut self) -> &mut AsAgentData {
+        &mut self.data
+    }
+
+    fn process(&mut self, _ctx: AgentContext, data: AgentData) -> Result<(), AgentError> {
+        let value = AgentValue::new_object(AgentValueMap::from([
+            ("kind".to_string(), AgentValue::new_string(data.kind)),
+            ("value".to_string(), data.value),
+        ]));
+        let json =
+            serde_json::to_value(&value).map_err(|e| AgentError::InvalidValue(e.to_string()))?;
+        let ctx = AgentValue::from_json_value(json)?;
+        let debug_data = AgentData::new_object(AgentValueMap::from([
+            ("ctx".to_string(), ctx),
+            ("data".to_string(), value),
+        ]));
+        self.emit_display(DISPLAY_DATA, debug_data);
+        Ok(())
+    }
+}
+
+static KIND: &str = "agent";
+static CATEGORY: &str = "Core/Display";
+
+static DISPLAY_DATA: &str = "data";
+
+pub fn register_agents(askit: &ASKit) {
+    // Display Data Agent
+    askit.register_agent(
+        AgentDefinition::new(KIND, "$display_data", Some(new_boxed::<DisplayDataAgent>))
+            .with_title("Display Data")
+            .with_category(CATEGORY)
+            .with_inputs(vec!["data"])
+            .with_display_config(vec![(
+                DISPLAY_DATA.into(),
+                AgentDisplayConfigEntry::new("*").with_hide_title(),
+            )]),
+    );
+
+    // Debug Data Agent
+    askit.register_agent(
+        AgentDefinition::new(KIND, "$debug_data", Some(new_boxed::<DebugDataAgent>))
+            .with_title("Debug Data")
+            .with_category(CATEGORY)
+            .with_inputs(vec!["*"])
+            .with_display_config(vec![(
+                DISPLAY_DATA.into(),
+                AgentDisplayConfigEntry::new("object").with_hide_title(),
+            )]),
+    );
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use agent_stream_kit::ASKit;
 
 pub mod counter;
+pub mod display;
 pub mod input;
 
 pub fn register_agents(askit: &ASKit) {
     counter::register_agents(askit);
+    display::register_agents(askit);
     input::register_agents(askit);
 }
